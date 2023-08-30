3fdcd374e0c07cfbe1f689a71f5c04d1ebeb9df5
Nguyễn Thành Tâm <nguyenthanhtam12b3@gmail.com>
2023-08-30 08:03:33 +0700

    fix import

--- firebase_example/lib/src/screens/home_screen.dart
+++ firebase_example/lib/src/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_example/src/constant/route_name.dart';
+import '../constant/route_name.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
